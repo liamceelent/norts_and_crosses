9b05fee5ebf8712cb562f1443d08effc851c3755
Liam Ceelen-Thomas <lce26@cs23144fw.canterbury.ac.nz>
2023-07-20 10:53:22 +1200

    fully workign base version of game

--- game.py
+++ game.py
@@ -1,17 +1,7 @@
 from random import randint 
 
-positions = {1: -1, 2: -1, 3: -1, 4: -1, 5: -1, 6: -1, 7: -1, 8: -1, 9: -1}
-
 win_options = {1: [[2,3], [5,9], [4,7]], 2: [[1,3], [5,8]], 3: [[1,2], [5,7], [6,9]], 4: [[1,7], [5,6]], 5: [[2,8], [4,6], [1,9], [3,7]], 6: [[3,9], [4,5]],\
            7: [[1,4], [8,9], [3,5]], 8: [[2,5], [7,9]], 9: [[3,6], [1,5], [7,8]]}
-
-
-def options(places):
-    playable = []
-    for i in places:
-        if places[i] == -1:
-            playable.append(i)
-    return playable 
 
 def win_check(win_options, chosenplayer, choice, player):
     for i in win_options[choice]:
@@ -23,27 +13,38 @@ def win_check(win_options, chosenplayer, choice, player):
             return True
     return False
 
+def bot_move(chosenbot, avaliable):
+    randomchoice = randint(1,len(avaliable))
+    chosenbot.append(avaliable[randomchoice-1])
+    avaliable.remove(avaliable[randomchoice-1])
+    return randomchoice
+
+
+
+
+avaliable = [1,2,3,4,5,6,7,8,9]
 
 chosenbot = []
 chosenplayer = []
 end = False
 
-while -1 in positions.values() and not end:
+while len(avaliable) > 0 and not end:
     print("please pick a location")
-    for i in options(positions):
+    for i in avaliable:
         print(i)
-    choice = int(input())
+    while True:
+        choice = int(input()) 
+        if choice not in avaliable:
+            print("select a avaliable square")
+        else:
+            break
+    avaliable.remove(choice)
     chosenplayer.append(choice)
-    positions[choice] = 1
 
     if win_check(win_options, chosenplayer, choice, 1):
         break
 
-
-    while True:
-        a
-        if a not in chosenplayer:
-            chosenbot.append(a)
-            positions[a] = 0
-        end = end | win_check(win_options, chosenplayer, choice, 0)
-    print("hi")
+    end = end | win_check(win_options, chosenbot, bot_move(chosenbot, avaliable), 0)
+    print(chosenplayer)
+    print(chosenbot)
+    print(avaliable)
