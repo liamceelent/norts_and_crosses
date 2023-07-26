1bd77c6ef88607b73081bdd5648901dd6c837213
Liam Ceelen-Thomas <lce26@cs23148fw.canterbury.ac.nz>
2023-07-27 10:48:15 +1200

    prototype

--- game_visuals.py
+++ game_visuals.py
@@ -1,42 +1,46 @@
-import pygame    
+import pygame  
+import sys  
     
-pygame.init()    
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
+screen = pygame.display.set_mode((1080, 600))
+width = screen.get_width()
+height = screen.get_height()
+color_light = (170,170,170)
+
+
+color_dark = (100,100,100)
 running = True
 dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    mouse = pygame.mouse.get_pos()
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+    pygame.draw.rect(screen, color_dark, [width/2-150, height/2, 100, 100])
+    pygame.draw.rect(screen, color_dark, [width/2+150, height/2, 100, 100])
+    pygame.draw.rect(screen, color_dark, [width/2, height/2+150, 100, 100])
+    pygame.draw.rect(screen, color_dark, [width/2, height/2-150, 100, 100])
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+    pygame.draw.rect(screen, color_dark, [width/2-150, height/2-150, 100, 100])
+    pygame.draw.rect(screen, color_dark, [width/2+150, height/2+150, 100, 100])
+    pygame.draw.rect(screen, color_dark, [width/2-150, height/2+150, 100, 100])
+    pygame.draw.rect(screen, color_dark, [width/2+150, height/2-150, 100, 100])
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
+    if width/2 <= mouse[0] <= width/2+100 and height/2 <= mouse[1] <= height/2+100:
+        for ev in pygame.event.get():
+            if ev.type == pygame.MOUSEWHEEL:
+                pygame.draw.rect(screen, "blue", [width/2, height/2, 100, 100])
+                pygame.display.update()
+            else:
+                pygame.draw.rect(screen, "red", [width/2, height/2, 100, 100])
+                pygame.display.update()
+    else:
+        pygame.draw.rect(screen, color_light, [width/2, height/2, 100, 100])
+
+    pygame.display.update()
 
 pygame.quit()
